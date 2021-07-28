856d66bf59af66927effb5c5342805e51c516401
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-28 22:12:59 +0100

    init: set up with initial boilerplate

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>Thiis is my boilerplate</p>, document.getElementById('app'));
