1fe8f518bd7a8699dd7a95ec4aa228a8b191b61c
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-31 17:27:07 +0100

    add: set up expensify redux store with multiple reducers

---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,52 @@
+import { createStore, combineReducers } from 'redux';
+
+// Expenses Reducer
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+// Filters Reducer
+const filtersReducerDefaultState = {
+	text: '',
+	sortBy: 'date',
+	startDate: undefined,
+	endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
+
+// Store Creation
+const store = createStore(
+	combineReducers({
+		expenses: expensesReducer,
+		filters: filtersReducer
+	})
+);
+
+console.log(store.getState());
+
+const demoState = {
+	expenses: [{
+		id: 'hsdausdhuias',
+		description: 'January Rent',
+		note: 'This was the final payment for that address',
+		amount: 54500,
+		createdAt: 0
+	}],
+	filters: {
+		text: 'rent',
+		sortBy: 'amount', // date or amount
+		startDate: undefined,
+		endDate: undefined
+	}
+};
