dd03e73ad52c4aa2dd23b3c5d3deafa34d1213a1
Andy Bradbury <andy@thestartupguys.co.uk>
2021-07-31 09:30:55 +0100

    add: experiment with destructuring arrays

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object Destructuring
+//
+
 // const person = {
 // 	name: 'Andy',
 // 	age: 35,
@@ -15,13 +19,25 @@
 // 	console.log(`It's ${temp} in ${city}.`);
 // }
 
-const book = {
-	title: 'Ego is the Enemy',
-	author: 'Ryan Holiday',
-	publisher: {
-		name: 'Penguin'
-	}
-};
+// const book = {
+// 	title: 'Ego is the Enemy',
+// 	author: 'Ryan Holiday',
+// 	publisher: {
+// 		name: 'Penguin'
+// 	}
+// };
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
-console.log(publisherName);
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+// console.log(publisherName);
+
+//
+// Array Destructuring
+//
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city}, ${state}.`);
+
+const item = ['Coffee (hot)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}.`);
