b54fa329ac2ec7c56414cc3baf555af47bc82288
Andy Bradbury <andy@thestartupguys.co.uk>
2021-08-01 19:41:19 +0100

    add: ability to sort

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -127,12 +127,20 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 // Get Visible Expenses
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
 	return expenses.filter((expense) => {
+		// If the dates aren't a number (i.e. 'undefined') then we're going to ignore the filter input and set the match to true.
+		// If it is a number, then we're going to check against it.
 		const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
 		const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
 		const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
 		return startDateMatch && endDateMatch && textMatch;
-	});;
+	}).sort((a, b) => {
+		if (sortBy === 'date') {
+			return a.createdAt < b.createdAt ? 1 : -1;
+		} else if (sortBy === 'amount') {
+			return a.amount < b.amount ? 1 : -1;
+		}
+	});
 };
 
 // Store Creation
@@ -149,16 +157,16 @@ store.subscribe(() => {
 	console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
 // store.dispatch(setStartDate(0));
