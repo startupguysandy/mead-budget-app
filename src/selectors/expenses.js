713402edbef930a4d868c0976fe64824dd6b6f36
Andy Bradbury <andy@thestartupguys.co.uk>
2021-08-11 14:49:46 +0100

    add: functionality to filter by dates, includong changing ExpenseListFilters to a class based component

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,10 +1,13 @@
+import moment from 'moment';
+
 // Get Visible Expenses
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
 	return expenses.filter((expense) => {
 		// If the dates aren't a number (i.e. 'undefined') then we're going to ignore the filter input and set the match to true.
 		// If it is a number, then we're going to check against it.
-		const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-		const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+		const createdAtMoment = moment(expense.createdAt);
+		const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+		const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
 		const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
 		return startDateMatch && endDateMatch && textMatch;
